09d86aef851eea0f89556cd5de63d7f2a0307ee1
Julien Willette <julienwillettetekyn.com>
2021-07-07 12:51:32 +0200

    add login_page html and css

---
+++ src/View/login_page.php
@@ -0,0 +1,16 @@
+<?php require 'index.php'; ?>
+
+<div class="div-login">
+    <h2>Enter your username</h2>
+    <form>
+        <span method="post">
+            <input type="text" name="username" placeholder="Username" autocomplete="off">
+        </span>
+        <span>
+            <a href="#">Créer un compte</a>
+        </span>
+        <span>
+            <input type="submit" value="Validé">
+        </span>
+    </form>
+</div>
