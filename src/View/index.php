fa023d0e2a44d127858de1273ebacfe35cca8e4c
Julien Willette <julienwillettetekyn.com>
2021-07-07 11:21:16 +0200

    init project

---
+++ src/View/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/asset/style.css">
+    <title>tekyn_tchat</title>
+</head>
+<body>
+    <h1>Hello world !</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dolore modi ea animi corporis reprehenderit aperiam voluptatum alias optio possimus ad, quod harum vitae earum quia inventore. Vero, optio quidem.</p>
+</body>
+</html>
