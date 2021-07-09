da0dff1eba93d66f319387796f05dfa077c7d175
Julien Willette <julienwillettetekyn.com>
2021-07-09 20:11:39 +0200

    add js function for send message no page refresh for send

--- src/View/all-tchat.php
+++ src/View/all-tchat.php
@@ -38,42 +38,25 @@
                     <p>11:11am Today</p>
                 </div>
             </div>
-            <div class="receive-msg">
-                <span class="line-tchat-2">
-                    <div class="color-send-message-2">
-                        <p>Good thanks </p>
-                    </div>
-                    <img src="../../public/images/Florence%20Hale.jpg" alt="Florence_Hale" class="img-tchat-2">
-                </span>
-                <div class="date-send-2">
-                    <p>11:20am Today</p>
-                </div>
-                <span class="line-tchat">
-                    <img src="../../public/images/Florence%20Hale.jpg" alt="Florence_Hale" class="img-tchat-1">
-                    <div class="color-send-message-1">
-                        <p>Hi how are you ? </p>
-                    </div>
-                </span>
-                <div class="date-send-1">
-                    <p>11:11am Today</p>
-                </div>
-            </div>
-            <div class="receive-msg">
-                <span class="line-tchat-2">
-                    <div class="color-send-message-2">
-                        <p>Good thanks </p>
-                    </div>
-                    <img src="../../public/images/Florence%20Hale.jpg" alt="Florence_Hale" class="img-tchat-2">
-                </span>
-                <div class="date-send-2">
-                    <p>11:20am Today</p>
-                </div>
-            </div>
         </span>
-    <span>
-            <form>
-                <input type="text" placeholder="Type your messages ..." class="type-message">
-                <input type="submit" name="message" autocomplete="off" value="validé" class="btn-message">
-            </form>
+        <span class="sendbox">
+            <input type="text" placeholder="Type your messages ..." class="type-message">
+            <input type="submit" name="message" autocomplete="off" value="validé" class="submit">
         </span>
 </div>
+
+<script>
+    submit=document.querySelector(".submit");
+    text=document.querySelector(".sendbox input");
+    chatbox=document.querySelector(".midle-tchat");
+    submit.addEventListener("click",sendmessage)
+
+    function sendmessage()
+    {
+        data=text.value.trim()
+        if(data!="")
+            chatbox.innerHTML+='<div class="eachmessage sent animated"><p>'+data+'</p></div>'
+        text.value=""
+        chatbox.scrollTop=chatbox.scrollHeight
+    }
+</script>
